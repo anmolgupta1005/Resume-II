2722ab0cbbc7580f062a6bda718b5434a47cf08a
agupta6 <anmol.gupta@analog.com>
2019-08-23 11:39:53 -0700

    Added Deployment. Pushes Resume back to repo as a release

--- scripts/latex_compiler.sh
+++ scripts/latex_compiler.sh
@@ -5,8 +5,9 @@ set -ev
 
 CONTAINER_NAME="Resume_Builder"
 IMAGE_NAME="latex_compiler"
+OUTPUT_NAME="Resume_Anmol_Gupta.pdf"
 LATEX_BUILD="sudo latexmk -cd -f -lualatex -interaction=nonstopmode -synctex=1 \
-  -outdir=/data/out -auxdir=/data/build contents/cv.tex"
+  -jobname=$OUTPUT_NAME -outdir=/data/out -auxdir=/data/build contents/cv.tex"
 
 docker run -d --rm --name $CONTAINER_NAME -t --user="$(id -u):$(id -g)" \
-  --net=none -v $PWD:/data $IMAGE_NAME /bin/sh -c $LATEX_BUILD
+  --net=none -v $PWD:/data $IMAGE_NAME "/bin/sh -c $LATEX_BUILD"
