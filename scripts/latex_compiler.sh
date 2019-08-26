3cc20bfe2ea43595378f63909653450e015a438a
agupta6 <anmol.gupta@analog.com>
2019-08-26 10:32:10 -0700

    typo fix in directory name

--- scripts/latex_compiler.sh
+++ scripts/latex_compiler.sh
@@ -7,7 +7,8 @@ CONTAINER_NAME="Resume_Builder"
 IMAGE_NAME="latex_compiler"
 OUTPUT_NAME="Resume_Anmol_Gupta.pdf"
 LATEX_BUILD="sudo latexmk -cd -f -lualatex -interaction=nonstopmode -synctex=1 \
-  -jobname=$OUTPUT_NAME -outdir=/data/out -auxdir=/data/build contents/cv.tex"
+-jobname=$OUTPUT_NAME -outdir=/data/out -auxdir=/data/build \
+/data/contents/cv.tex"
 
 docker run -d --rm --name $CONTAINER_NAME -t --user="$(id -u):$(id -g)" \
-  --net=none -v $PWD:/data $IMAGE_NAME "/bin/sh -c $LATEX_BUILD"
+--net=none -v $PWD:/data $IMAGE_NAME $LATEX_BUILD
